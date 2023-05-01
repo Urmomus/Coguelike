023bc4e6abbfd34ab33135251830b95b3076ee32
Sh4vka <126407063+Sh4vka@users.noreply.github.com>
2023-05-02 00:55:00 +0300

    sorry

---
+++ view/view.c
@@ -0,0 +1,28 @@
+#include 'Map.h'
+#include <ncurses.h>
+
+int show_map(map)
+{
+    if getch
+    initscr();
+    //getch('M');
+    printw(map);
+    endwin();
+}
+
+int show_menu()
+{
+    initscr();
+
+    printw("Continue\n","Exit\n");
+    
+    endwin();
+}
+
+int show_inventor(items)
+{
+    intscr();
+
+    printw(items)
+    endwin()
+}
